59c3ddb99ad68b074e7b358147ef1895a59ec183
luiz felipe <luizfpereira406@gmail.com>
2023-05-08 16:14:29 -0300

    feat: Add comment on how to change between implementations

--- repositories/repositories.go
+++ repositories/repositories.go
@@ -2,12 +2,15 @@ package repositories
 
 import (
 	"github.com/LuizFelipe406/Database-With-GO/repositories/users"
+	"github.com/jinzhu/gorm"
 	"github.com/jmoiron/sqlx"
 )
 
 type Options struct {
 	WriterSqlx *sqlx.DB
 	ReaderSqlx *sqlx.DB
+	WriterGorm *gorm.DB
+	ReaderGorm *gorm.DB
 }
 
 type Container struct {
@@ -16,6 +19,6 @@ type Container struct {
 
 func New(options Options) *Container {
 	return &Container{
-		User: users.NewSqlxRepo(options.WriterSqlx, options.ReaderSqlx),
+		User: users.NewSqlxRepo(options.WriterSqlx, options.ReaderSqlx), // Modify to users.NewGormRepo(options.WriterGorm, options.ReaderGorm) to change between implementations
 	}
 }
